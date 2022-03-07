15555cb2d54d676317de7df95590a6155a05e2aa
shenjr <1012198731@qq.com>
2022-03-07 09:14:20 +0800

    [feat] 新增docker打包方式

--- service-user/src/main/java/com/nutritious/meal/UserApplication.java
+++ service-user/src/main/java/com/nutritious/meal/UserApplication.java
@@ -2,6 +2,9 @@ package com.nutritious.meal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * description: 用户启动类
@@ -10,9 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * create_date: 2022/3/3 14:03
  **/
 @SpringBootApplication
+@RestController
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @GetMapping("/{id}")
+    public String get(@PathVariable("id") String id) {
+        return "Hello World, " + id;
     }
 }
